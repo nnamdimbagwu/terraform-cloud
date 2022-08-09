44d0f6db9d10e00fc1d1fd549a7cc9852bb55c18
nnamdimbagwu <89325455+nnamdimbagwu@users.noreply.github.com>
2022-08-09 01:15:14 -0400

    Update main.tf
    Added another subnet

--- main.tf
+++ main.tf
@@ -24,3 +24,14 @@ resource "azurerm_subnet" "subnet" {
     ]
 }
 
+resource "azurerm_subnet" "subnet" {    
+    name                 = "SubnetB"
+    resource_group_name  = local.resource_group_name
+    virtual_network_name = local.virtual_network.name
+    address_prefixes     = [cidrsubnet(local.virtual_network.address_space,8,1)]
+    depends_on = [
+      azurerm_virtual_network.network
+    ]
+}
+
+
